c020d974e597719a025be3efee38c9e4048049a5
Michael Kainer <stuff@pushrax.com>
2019-08-18 21:39:45 +0200

    Blender exporter

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Instruction sets: MMX(+), SSE, SSE2, SSSE3, SSE4.1, SSE4.2, AVX, AVX2, FMA
+
+Export from Blender with:
+  blender .\examples\cube.blend -b -P blender_ray_exporter.py
---
+++ blender_ray_exporter.py
@@ -0,0 +1,95 @@
+import bpy
+import os
+import sys
+import json
+import itertools
+from math import sqrt
+from mathutils import Matrix, Vector, Euler, Quaternion
+
+def convert_color(c):
+    return [c.r, c.g, c.b]
+
+def convert_prop_array(arr):
+    return list(arr)
+
+def convert_vector(v):
+    return list(v)
+
+def convert_matrix(m):
+    return [[c for c in r] for r in m]
+
+def rename(name):
+    return name.replace(" ", "_").lower()
+
+def main():
+    export_path = bpy.data.filepath + ".json"
+    print("Exporting to: " + export_path)
+    print()
+
+    out_objects = {} 
+    depsgraph = bpy.context.evaluated_depsgraph_get()
+    for object_inst in depsgraph.object_instances:
+        object = object_inst.object
+        print(f">>> Object {object.type} {object.name}")
+
+        out_object = dict()
+        out_object["name"] = object.name
+        out_object["type"] = object.type
+
+        if object.type == "MESH":
+            mesh = object.to_mesh()
+            mesh.calc_loop_triangles()
+            print(f"\t{len(mesh.loop_triangles)} triangles")
+            triangles = []
+            for t in mesh.loop_triangles:
+                for v in t.vertices:
+                    v = mesh.vertices[v]
+                    triangles.append({
+                        "p": convert_vector(v.co),
+                        "n": convert_vector(v.normal),
+                    })
+            object.to_mesh_clear()
+            out_object["triangles"] = triangles
+
+            material = object.active_material
+            out_material = dict()
+            out_object["material"] = out_material
+            out_material["name"] = material.name
+            material_output_node = material.node_tree.nodes['Material Output']
+            surface_node = material_output_node.inputs["Surface"].links[0].from_node
+            out_material["type"] = surface_node.type
+            for (in_name, in_value) in surface_node.inputs.items():
+                if in_value.type == "VALUE":
+                    out_material[rename(in_name)] = in_value.default_value
+                elif in_value.type == "RGBA":
+                    out_material[rename(in_name)] = convert_prop_array(in_value.default_value)
+                elif in_value.type == "VECTOR":
+                    out_material[rename(in_name)] = convert_vector(in_value.default_value)
+                else:
+                    print("\tUnknown type in material node", in_value.type)
+        elif object.type == "LIGHT":
+            out_object["lamp_type"] = object.data.type
+            out_object["color"] = convert_color(object.data.color)
+            out_object["power"] = object.data.energy
+            out_object["specular"] = object.data.specular_factor
+            out_object["radius"] = object.data.shadow_soft_size
+            out_object["attenuation"] = [0.00111109, 0.0, 1.0]
+            out_object["matrix"] = convert_matrix(object.matrix_world)
+        elif object.type == "CAMERA":
+            out_object["matrix"] = convert_matrix(object.matrix_world)
+            out_object["xfov"] = object.data.angle_x
+            out_object["yfov"] = object.data.angle_y
+            out_object["znear"] = object.data.clip_start
+            out_object["nfar"] = object.data.clip_end
+            out_object["camera_type"] = object.data.type
+
+        out_objects[object.name] = out_object
+
+    out = {
+        "objects": out_objects,
+    }
+
+    with open(export_path, "w", encoding="ASCII") as fp:
+        json.dump(out, fp, indent=2)
+
+main()
