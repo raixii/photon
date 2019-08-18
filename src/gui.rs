7b0b06d9ebdb5d2b2dd34cd0cae7a4b235824382
Michael Kainer <stuff@pushrax.com>
2019-08-18 14:06:49 +0200

    Change UI code, add arguments, make exposure changeable

---
+++ src/gui.rs
@@ -0,0 +1,72 @@
+use crate::image_buffer::ImageBuffer;
+use crate::math::Vec4;
+use minifb::{Key, KeyRepeat, Window, WindowOptions};
+use std::sync::atomic::{AtomicBool, Ordering::Relaxed};
+use std::{sync::Mutex, thread, time};
+
+pub fn main_loop(
+    window_w: usize,
+    window_h: usize,
+    mut exposure: f64,
+    image: &Mutex<ImageBuffer>,
+    want_quit: &AtomicBool,
+) {
+    let mut float_buffer = vec![Vec4([0.0; 4]); window_w * window_h];
+    let mut display_buffer = vec![0; window_w * window_h];
+    let mut buffer_version = std::usize::MAX;
+    let mut buffer_changed = true;
+
+    let mut window = Window::new("Photon", window_w, window_h, WindowOptions::default()).unwrap();
+    while window.is_open() {
+        {
+            let image = image.lock().unwrap();
+            if buffer_version != image.version() {
+                float_buffer.copy_from_slice(image.get_buffer());
+                buffer_version = image.version();
+                buffer_changed = true;
+            }
+        }
+
+        if let Some(pressed_keys) = window.get_keys_pressed(KeyRepeat::No) {
+            let shift_down =
+                window.is_key_down(Key::LeftShift) || window.is_key_down(Key::RightShift);
+            for pressed_key in pressed_keys {
+                match pressed_key {
+                    Key::F3 => {
+                        exposure -= if shift_down { 0.1 } else { 1.0 };
+                        buffer_changed = true;
+                    }
+                    Key::F4 => {
+                        exposure += if shift_down { 0.1 } else { 1.0 };
+                        buffer_changed = true;
+                    }
+                    _ => {}
+                }
+            }
+        }
+
+        if buffer_changed {
+            for (i, color) in float_buffer.iter().enumerate() {
+                let mut c = color.xyz();
+                for i in 0..3 {
+                    c.0[i] *= 2.0f64.powf(exposure); // exposure
+                    c.0[i] /= 1.0 + c.0[i]; // tone mapping (Reinhard)
+                    c.0[i] = c.0[i].min(1.0).max(0.0); // clamp between [0; 1]
+                    c.0[i] = c.0[i].powf(2.2); // gamma correction
+                    c.0[i] *= 255.0; // machine numbers
+                }
+                display_buffer[i] =
+                    ((c.0[0] as u32) << 16) | ((c.0[1] as u32) << 8) | (c.0[2] as u32);
+            }
+            window.update_with_buffer(&display_buffer).unwrap();
+            window.set_title(&format!("Photon: exposure={:+.1}", exposure));
+            buffer_changed = false;
+        } else {
+            window.update();
+        }
+
+        thread::sleep(time::Duration::from_millis(16));
+    }
+
+    want_quit.store(true, Relaxed);
+}
--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,18 @@
 extern crate clap;
 
 use bvh::Bvh;
-use minifb::{Window, WindowOptions};
-use scene::Scene;
+use image_buffer::ImageBuffer;
 use std::fmt::{Debug, Formatter};
-use std::{fs, io::Read, str::FromStr, sync::atomic, sync::mpsc, sync::Arc, thread, time};
+use std::{fs, io::Read, str::FromStr, sync::atomic, sync::Arc, sync::Mutex, thread, time};
 use tracing::raytrace;
 
 mod bvh;
 mod collada;
+mod gui;
+mod image_buffer;
 mod math;
 mod scene;
 mod tracing;
-
-struct ThreadData {
-    pub scene: Scene,
-    pub pixel_at: atomic::AtomicUsize,
-    pub want_quit: atomic::AtomicBool,
-    pub max_thread_time: atomic::AtomicU64,
-    pub running_threads: atomic::AtomicUsize,
-}
 
 struct ErrorMessage(String);
 
@@ -45,6 +38,7 @@ impl From<&str> for ErrorMessage {
 }
 
 fn main() -> Result<(), ErrorMessage> {
+    let cpu_count_str = format!("{}", num_cpus::get());
     let clap_app = clap_app!(photon =>
         (version: crate_version!())
         (author: crate_authors!("; "))
@@ -52,22 +46,18 @@ fn main() -> Result<(), ErrorMessage> {
         (@arg INPUT: +required "DAE file to render")
         (@arg OUTPUT: +required "PNG file to write")
         (@arg headless: -H --headless "Do not show the GUI")
-        (@arg threads: -t --threads +takes_value "Number of worker threads")
+        (@arg threads: -t --threads +takes_value default_value(&cpu_count_str) "Number of worker threads")
+        (@arg exposure: -e --exposure +takes_value default_value("0.0") "Exposure multiplier of the camera given as a power of two")
+        (@arg width: -x --width +takes_value default_value("1600") "Image width in pixels")
+        (@arg height: -y --height +takes_value default_value("900") "Image height in pixels")
     );
     let matches = clap_app.get_matches();
-    let thread_count = if let Some(tc) = matches.value_of("threads") {
-        let tc = FromStr::from_str(tc).map_err(|_| "--threads expects a number.")?;
-        if tc < 1 {
-            Err("--threads must be at least 1.")?;
-        }
-        tc
-    } else {
-        num_cpus::get()
-    };
-    let window_w = 1600;
-    let window_h = 900;
+    let thread_count = FromStr::from_str(matches.value_of("threads").unwrap()).unwrap();
+    let window_w = FromStr::from_str(matches.value_of("width").unwrap()).unwrap();
+    let window_h = FromStr::from_str(matches.value_of("height").unwrap()).unwrap();
+    let exposure = FromStr::from_str(matches.value_of("exposure").unwrap()).unwrap();
 
-    let scene = {
+    let scene = Arc::new({
         let start_time = time::Instant::now();
         let path = matches.value_of("INPUT").unwrap();
         let mut infile =
@@ -88,105 +78,51 @@ fn main() -> Result<(), ErrorMessage> {
         eprintln!("Building BVH: {} ms", (end_time - start_time).as_millis());
 
         scene
+    });
+
+    let image_buffer = Arc::new(Mutex::new(ImageBuffer::new(window_w, window_h)));
+    let want_quit = Arc::new(atomic::AtomicBool::new(false));
+    let pixel_at = Arc::new(atomic::AtomicUsize::new(0));
+
+    let window_thread = {
+        let image_buffer = Arc::clone(&image_buffer);
+        let want_quit = Arc::clone(&want_quit);
+        thread::spawn(move || {
+            gui::main_loop(window_w, window_h, exposure, &image_buffer, &want_quit);
+        })
     };
 
-    let (sender, receiver) = mpsc::channel();
-    let thread_data = Arc::new(ThreadData {
-        scene,
-        pixel_at: atomic::AtomicUsize::new(0),
-        want_quit: atomic::AtomicBool::new(false),
-        max_thread_time: atomic::AtomicU64::new(0),
-        running_threads: atomic::AtomicUsize::new(thread_count),
-    });
-    let join_handles: Vec<_> = (0..thread_count)
-        .map(|_| {
-            let my_thread_data = Arc::clone(&thread_data);
-            let my_sender = sender.clone();
-            thread::spawn(move || {
-                let start_time = time::Instant::now();
-                while !my_thread_data.want_quit.load(atomic::Ordering::Relaxed) {
-                    let my_pixel = my_thread_data.pixel_at.fetch_add(1, atomic::Ordering::Relaxed);
-                    let (my_x, my_y) = (my_pixel % window_w, my_pixel / window_w);
-                    if my_y >= window_h {
-                        break;
-                    }
-                    let color = raytrace(
-                        &my_thread_data.scene,
-                        my_x as f64,
-                        my_y as f64,
-                        window_w as f64,
-                        window_h as f64,
-                    );
-                    if let Some(color) = color {
-                        my_sender.send((my_x, my_y, color)).unwrap();
-                    }
+    let start_time = time::Instant::now();
+    let mut worker_threads = Vec::with_capacity(thread_count);
+    for _t in 0..thread_count {
+        let scene = Arc::clone(&scene);
+        let want_quit = Arc::clone(&want_quit);
+        let pixel_at = Arc::clone(&pixel_at);
+        let image_buffer = Arc::clone(&image_buffer);
+        let worker_thread = thread::spawn(move || {
+            while !want_quit.load(atomic::Ordering::Relaxed) {
+                let my_pixel = pixel_at.fetch_add(1, atomic::Ordering::Relaxed);
+                let (my_x, my_y) = (my_pixel % window_w, my_pixel / window_w);
+                if my_y >= window_h {
+                    break;
                 }
-                let end_time = time::Instant::now();
-
-                let mut stored_time =
-                    my_thread_data.max_thread_time.load(atomic::Ordering::Relaxed);
-                loop {
-                    match my_thread_data.max_thread_time.compare_exchange_weak(
-                        stored_time,
-                        stored_time.max((end_time - start_time).as_millis() as u64),
-                        atomic::Ordering::Relaxed,
-                        atomic::Ordering::Relaxed,
-                    ) {
-                        Ok(_) => break,
-                        Err(x) => stored_time = x,
-                    }
+                let color =
+                    raytrace(&scene, my_x as f64, my_y as f64, window_w as f64, window_h as f64);
+                if let Some(color) = color {
+                    let mut buffer = image_buffer.lock().unwrap();
+                    buffer.set_pixel(my_x, my_y, color.xyz1());
                 }
-
-                my_thread_data.running_threads.fetch_sub(1, atomic::Ordering::Relaxed);
-            })
-        })
-        .collect();
-
-    let mut buffer = vec![0; window_w * window_h];
-    for x in 0..window_w {
-        for y in 0..window_h {
-            buffer[y * window_w + x] =
-                if (x / 32) % 2 == (y / 32) % 2 { 0xFF_FF_FF } else { 0xEE_EE_EE }
-        }
-    }
-    let mut window = Window::new("Photon", window_w, window_h, WindowOptions::default())
-        .map_err(|_| "Cannot open the window.")?;
-    let mut printed_time = false;
-    while window.is_open() {
-        for (x, y, mut color) in receiver.try_iter() {
-            for i in 0..3 {
-                /*
-                color.0[i] *= 1.0 / 800.0; // exposure
-                                           //color.0[i] = color.0[i] / (1.0 + color.0[i]); // tone-mapping
-                                           //  color.0[i] = color.0[i].min(1.0);
-                color.0[i] = (0.0f64.max(color.0[i] - 0.004));
-                color.0[i] = (color.0[i] * (6.2 * color.0[i] + 0.5))
-                    / (color.0[i] * (6.2 * color.0[i] + 1.7) + 0.06);
-                //   color.0[i] = color.0[i].powf(1.0 / 2.2); // gamma correction
-                color.0[i] *= 255.0; // machine numbers
-                // color.0[i] = (color.0[i] / (1.0 + color.0[i])).powf(2.2);
-                */
-                color.0[i] *= 255.0; // machine numbers
-                color.0[i] /= 1000.0;
             }
-            buffer[y * window_w + x] =
-                ((color.x() as u32) << 16) | ((color.y() as u32) << 8) | (color.z() as u32);
-        }
-        window.update_with_buffer(&buffer).map_err(|_| "Cannot update the window.")?;
-        thread::sleep(time::Duration::from_millis(250));
-
-        if !printed_time && thread_data.running_threads.load(atomic::Ordering::Relaxed) == 0 {
-            eprintln!(
-                "Raytracing: {} ms",
-                thread_data.max_thread_time.load(atomic::Ordering::Relaxed)
-            );
-            printed_time = true;
-        }
+        });
+        worker_threads.push(worker_thread);
     }
-
-    thread_data.want_quit.store(true, atomic::Ordering::Relaxed);
-    for join_handle in join_handles {
-        join_handle.join().unwrap();
+    for worker_thread in worker_threads {
+        worker_thread.join().unwrap();
     }
+    let end_time = time::Instant::now();
+    eprintln!("Raytracing: {} ms", (end_time - start_time).as_millis());
+
+    window_thread.join().unwrap();
+
     Ok(())
 }
